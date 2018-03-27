09b2ebacacaecb23db5a6443908273b680777bc9
Nanette Girzi <ngirzi@gmail.com>
2018-03-27 16:22:22 -0700

    follow lesson

--- to-do/src/app/app.module.ts
+++ to-do/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
