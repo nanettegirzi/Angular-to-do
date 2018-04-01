2f5d3a073b09bf9c789a77b391a64b5eb1286268
Nanette Girzi <ngirzi@gmail.com>
2018-04-01 16:39:08 -0700

    follow lesson

--- to-do/src/app/app.component.ts
+++ to-do/src/app/app.component.ts
@@ -12,14 +12,14 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year:number = this.currentTime.getFullYear();
+  selectedTask: Task = null;
 
-  tasks: Task[] = [
+
+  masterTaskList: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-
-  selectedTask: Task = null;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
@@ -29,13 +29,4 @@ export class AppComponent {
     this.selectedTask = null;
   }
 
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2){
-      return "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
 }
